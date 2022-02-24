aeeba341ef65621d0db849a894dbc5404ab48d34
Gyuris Csaba <77810176+gycsaba-vasvari@users.noreply.github.com>
2022-02-24 09:43:18 +0100

    Update Tests.cs

--- tests/Tests.cs
+++ tests/Tests.cs
@@ -1,22 +1,11 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.IO;
-
-using RectangleProject.Models;
-
-namespace RectangleProject.Tests 
+namespace RectangleProject.Tests
 {
-
-
-
     [TestClass()]
     public class Tests
     {
-
+        [TestMethod()]
+        public void Test()
+        {
             Rectangle r = new Rectangle(4.25,7.34);
             double expectedSideA = 4.25;
             double actualSideA = r.SideA;
@@ -59,6 +48,7 @@ namespace RectangleProject.Tests
             Assert.IsTrue(haveSideB, "A ToString metódus nem jeleníti meg az egyik oldalt");
             Assert.IsTrue(haveParimeter, "A ToString metódus nem jeleníti meg a kerületet két tizedes jegy pontossággal!");
             Assert.IsTrue(haveArea, "A ToString metódus nem jeleníti meg a területet két tizedes jegy pontosságal.");
+
         }
     }
 }
